1b6bc58ae57188e8089d6abe3fda2489456ee79b
Tuğrul Yüce <tugrulyuce80@gmail.com>
2024-11-14 16:38:45 +0300

    detailresponsive

--- src/components/ProductDetailComp.jsx
+++ src/components/ProductDetailComp.jsx
@@ -2,7 +2,6 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 
@@ -12,15 +11,16 @@ const ProductDetailComp = ({ productDetail }) => {
     <Card
   sx={{
     display: "flex",
-    justifyContent: "center", // Card'ı yatayda ortalar
-    margin: "7rem auto", // Card'ı ekranda ortalamak için
+    justifyContent: "center",
+    margin: { xs: "0 auto", sm: "7rem auto" }, // Küçük ekranda yukarıdaki boşluk kaldırılır
     maxWidth: 1000,
-    height:400 // Kart genişliğini sınırlamak için
+    height: { xs: "auto", sm: 400 }, // Mobilde yükseklik otomatik ayarlanır
+    flexDirection: { xs: "column", sm: "row" }, // Küçük ekranlarda üst üste yerleşir
   }}
 >
   <CardMedia
     component="img"
-    sx={{ width: 350 }} // Görselin genişliğini daraltmak için
+    sx={{ width: { xs: "100%", sm: 350 } }} // Mobilde genişlik %100 olur
     image={productDetail.images}
     alt="Product Image"
   />
@@ -29,18 +29,25 @@ const ProductDetailComp = ({ productDetail }) => {
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
-      maxWidth: 650, // Yazıların olduğu kısmı daraltmak için
-      paddingLeft: 2,
+      maxWidth: { xs: "100%", sm: 650 }, // Mobilde tam genişlik
+      paddingLeft: { xs: 0, sm: 2 }, // Küçük ekranda padding kaldırılır
+      paddingTop: { xs: 2, sm: 0 }, // Küçük ekranda üstten boşluk eklenir
     }}
   >
-    <CardContent sx={{ flex: "1 0 auto", marginTop:"70px" }}>
-      <Typography component="div" sx={{ fontSize:"1.6rem",  }}>
+    <CardContent
+      sx={{ flex: "1 0 auto", marginTop: { xs: "0", sm: "70px" } }}
+    >
+      <Typography component="div" sx={{ fontSize: "1.6rem" }}>
         {productDetail.title}
       </Typography>
       <Typography
         variant="subtitle1"
         component="div"
-        sx={{ fontSize:"1.3rem", color: "text.secondary" ,margin:"12px 0" }}
+        sx={{
+          fontSize: "1.3rem",
+          color: "text.secondary",
+          margin: "12px 0",
+        }}
       >
         {productDetail.description}
       </Typography>
@@ -52,8 +59,11 @@ const ProductDetailComp = ({ productDetail }) => {
           readOnly
         />
       </Typography>
-      <Typography sx={{ fontSize:"25px", color: "blue" ,margin:"12px 0" }}>$ {productDetail.price}</Typography>
-     
+      <Typography
+        sx={{ fontSize: "25px", color: "blue", margin: "12px 0" }}
+      >
+        $ {productDetail.price}
+      </Typography>
     </CardContent>
   </Box>
 </Card>
